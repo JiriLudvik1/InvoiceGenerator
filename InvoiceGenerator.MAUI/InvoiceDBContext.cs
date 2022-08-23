226f07650a75cd510aac304e6caeac658654a7c0
Jiří Ludvík <jirka.ludvik1@seznam.cz>
2022-08-23 21:51:31 +0200

    Got rid of mac targetting. finally

--- InvoiceGenerator.MAUI/InvoiceDBContext.cs
+++ InvoiceGenerator.MAUI/InvoiceDBContext.cs
@@ -1,19 +1,20 @@
-﻿//using InvoiceGenerator.MAUI.Models;
+﻿using InvoiceGenerator.MAUI.Models;
+using Microsoft.EntityFrameworkCore;
 
-//namespace InvoiceGenerator.MAUI
-//{
-//  public class InvoiceDBContext : DbContext
-//  {
-//    public DbSet<Customer> Customers { get; set; }
+namespace InvoiceGenerator.MAUI
+{
+  public class InvoiceDBContext : DbContext
+  {
+    public DbSet<Customer> Customers { get; set; }
 
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//    {
-//      optionsBuilder.UseSqlite("Data Source=InvoiceGenerator.db;");
-//    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlite("Data Source=InvoiceGenerator.db;");
+    }
 
-//    protected override void OnModelCreating(ModelBuilder modelBuilder)
-//    {
-//      modelBuilder.Entity<Customer>().ToTable("Customers");
-//    }
-//  }
-//}
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Customer>().ToTable("Customers");
+    }
+  }
+}
